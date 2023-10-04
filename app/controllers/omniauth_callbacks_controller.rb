3c2a840f1abb3ec694b4c4c8d9a15f166eea899a
Tran Dat <visualbasic2013@hotmail.com>
2023-10-05 00:06:21 +0700

    Add some experiments with Omniauth-facebook

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,15 @@
 class OmniauthCallbacksController < ApplicationController
   def facebook
+    user = User.find_or_create_by(uid: auth[:uid], provider: auth[:provider]) do |user|
+      user.uid = auth[:uid]
+      user.provider = auth[:provider]
+      user.name = auth[:info][:name]
+      user.image = auth[:info][:image]
+    end
+    if user
+      session[:user_id] = user.uid
+      redirect_to root_path
+    end
   end
 
   def auth
